8e6358645fad7340e0b5e4f8d08db73bf70c59b7
alexis <mdalexisaguilera@gmail.com>
2019-08-01 16:42:19 -0300

    mod

--- scripts.js
+++ scripts.js
@@ -1,19 +1,25 @@
 
-let button = document.querySelector('#button');
+let button = document.querySelector('.button');
+let randomNumber = Math.floor((Math.random() * 10) + 1)
+const apiUrl = "https://corebiz.myvtex.com/api/catalog_system/pub/products/search/tablete";
 let title = document.querySelector('.title');
-let code = document.querySelector('.code');
-let link = document.querySelector('.link');
+let reference = document.querySelector('.reference');
 
-function getInfo() {
-  let randomNumber = Math.floor((Math.random() * 88) + 1)
-  let apiUrl = "https://corebiz.myvtex.com/api/catalog_system/pub/products/search/tablete";
-  axios.get(apiUrl).then(function(response){
-    updateInfo(response.data)
+
+let refId = document.querySelector('.refId');
+
+// button.addEventListener('click', getItem);
+
+function getItem() {
+  fetch(apiUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function(data){
+      refId.innerText = data[randomNumber].productId
+      title.innerText = data[randomNumber].productTitle
+      reference.innerText = data[randomNumber].productReference
   })
 }
 
-function updateInfo(data){
-  title.innerText = data.productName
-}
-
-button.addEventListener('click', getInfo);
+getItem();
